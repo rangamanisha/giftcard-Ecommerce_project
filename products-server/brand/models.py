b02cded487a3ce3977a8a57f4004388761eb0f55
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2021-04-09 17:58:40 +0530

    ADDED: Product server

---
+++ products-server/brand/models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.db import models
+
+
+class Brand(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(verbose_name='Name', max_length=52, unique=True)
+    description = models.TextField(verbose_name='Description',)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return '{}'.format(self.name)
