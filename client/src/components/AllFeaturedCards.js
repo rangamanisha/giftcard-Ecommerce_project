29c9fff14fcefc25dfa22851c7e38dd238d2fe38
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2021-04-10 02:10:17 +0530

    FIXED: Console errors

--- client/src/components/AllFeaturedCards.js
+++ client/src/components/AllFeaturedCards.js
@@ -13,6 +13,7 @@ import Allmenu from "../assets/allmenu.svg";
 
 const AllFeaturedCards = () => {
    
+  // eslint-disable-next-line
   const [pics, setPics] = useState([]);
   const [imglist, setImglist] = useState([]);
 
@@ -21,7 +22,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
     axios.get(apiUrl)
     .then((result) => {    
        const pics = result.data.data.brands;
-       const imglist = pics.map((img) => <img className="imgcards mr-sm-5 mt-5" src={img.images.color.small_rectangle} />)
+       const imglist = pics.map((img) => <img className="imgcards mr-sm-5 mt-5" alt="Icon" src={img.images.color.small_rectangle} />)
        setPics(pics);
        setImglist(imglist);
     });
@@ -35,7 +36,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
     .then((result) => {    
       ; 
        const pics = result.data.data.brands;
-       const imglist = pics.map((img) => <img className="imgcards mr-sm-5 mt-5" src={img.images.color.small_rectangle} />)
+       const imglist = pics.map((img) => <img className="imgcards mr-sm-5 mt-5" alt="Icon" src={img.images.color.small_rectangle} />)
        setPics(pics);
        setImglist(imglist);
     });
@@ -49,7 +50,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
     .then((result) => {    
       ; 
        const pics = result.data.data.brands;
-       const imglist = pics.map((img) => <img className="imgcards mr-sm-5 mt-5" src={img.images.color.small_rectangle} />)
+       const imglist = pics.map((img) => <img className="imgcards mr-sm-5 mt-5" alt="Icon" src={img.images.color.small_rectangle} />)
        setPics(pics);
        setImglist(imglist);
     });
@@ -64,12 +65,9 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
       <div className="mt-5 ">
         <div className="row" style={{marginLeft:'130px'}}>
         <div className="box">
-            <a>
-              
-            </a>
             </div>
           <div className="box">
-            <a>
+            <a href="/">
               <img src={Allmenu} onClick={Featuredcards} alt="Icon" style={{width:'30px',height:'30px'}}/>
               <br />
               <p className="products_icons">All Gift Cards</p>
@@ -77,7 +75,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
             </div>
 
               <div className="box">
-                <a>
+                <a href="/">
                   <img
                     src={Flowersandgift}
                     onClick={Flowers}
@@ -90,7 +88,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
               </div>
 
               <div className="box">
-                <a>
+                <a href="/">
                   <img src={Beauty} alt="Icon" onClick={Beautycards} style={{width:'30px',height:'30px'}}/>
                   <br />
                   <p className="products_icons">Beauty</p>
@@ -98,7 +96,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
               </div>
 
               <div className="box">
-                <a>
+                <a href="/">
                   <img src={Hypermarket} alt="Icon" style={{width:'30px',height:'30px'}}/>
                   <br />
                   <p className="products_icons">Hypermarket</p>
@@ -106,7 +104,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
               </div>
 
               <div className="box">
-                <a>
+                <a href="/">
                   <img src={Technology} alt="Icon" style={{width:'30px',height:'30px'}}/>
                   <br />
                   <p className="products_icons">Technology</p>
@@ -114,7 +112,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
               </div>
 
               <div className="box">
-                <a>
+                <a href="/">
                   <img src={Toys} alt="Icon" style={{width:'30px',height:'30px'}}/>
                   <br />
                   <p className="products_icons">Kids & toys</p>
@@ -123,7 +121,7 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
 
               <div className="box">
                 {" "}
-                <a>
+                <a href="/">
                   <img src={Suitcase} alt="Icon" style={{width:'30px',height:'30px'}}/>
                   <br />
                   <p className="products_icons">Travel & hotels</p>
@@ -131,16 +129,13 @@ const apiUrl = "https://api.giftiglobal.com/v1/brands/featured_brands?currency=1
               </div>
 
               <div className="box">
-                <a>
+                <a href="/">
                   <img src={Shipped} alt="Icon" style={{width:'30px',height:'30px'}}/>
                   <br />
                   <p className="products_icons">Transportation</p>
                 </a>
               </div>
               <div className="box">
-            <a>
-              
-            </a>
             </div>
         </div>
         <div className="cardgifiti-card">
