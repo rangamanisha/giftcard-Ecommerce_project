11eb68f1823a55eac5dee983bfa6cf46cdd7f985
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2021-04-09 23:34:29 +0530

    FIXED: docker-compose config

--- server/config.py
+++ server/config.py
@@ -9,6 +9,6 @@ host = "mongodb://root:root@db:27017/giftiglobal"
 EMAIL = "bhanuchander008@gmail.com"
 PASSWORD = "sabhi1015"
 subject = 'welcome to GFG world'
-message = f'Hi  thank you for registering in Gifti Global site.'
+message = 'Hi  thank you for registering in Gifti Global site.'
 bcc = "bhanuchander008@gmail.com"
 cc = "bhanu.c@winvest-global.com"
