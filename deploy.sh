dba0a1982ea04d47a6948bcb492f3abcc0ef10dd
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2021-04-17 00:59:37 +0530

    UPDATED: Deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,7 @@ ssh -o StrictHostKeyChecking=no ec2-user@$EC2_PUBLIC_IP_ADDRESS  << 'ENDSSH'
   docker login -u $CI_REGISTRY_USER -p $CI_JOB_TOKEN $CI_REGISTRY
   docker-compose down
   docker system prune -f
-  docker-compose up --build -d
+  docker image rm $IMAGE
+  docker pull $IMAGE:latest
+  docker-compose up -d
 ENDSSH
