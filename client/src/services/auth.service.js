a5aa17035d2179c88cfcf5263f0633885fc48a85
Sai Charan Krishnagiri <sai.charan1994@gmail.com>
2021-04-19 19:45:58 +0530

    UPDATED: Env variables

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import { apiCall, API_URL } from './api';
 
 export const loginAPI = (userData) => {
-  const url = `${API_URL}/login/`;
+  const url = `${API_URL}/accounts/sessions/signin`;
   return apiCall(url, 'POST', userData, null, false);
 };
 
