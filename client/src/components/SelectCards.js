6d260a0b30e0ef6117e12711ff8a8ec40efbb090
shreya_19 <shreya@visions.net.in>
2021-04-23 02:45:41 +0530

    functional_buttons

--- client/src/components/SelectCards.js
+++ client/src/components/SelectCards.js
@@ -8,42 +8,50 @@ import minusicon from '../assets/minus.svg';
 import radio from '../assets/radio.svg';
 import AmazonMedium from "../assets/amazon_medium.png";
 import Footer1 from './Stikyfooter';
-import {useDispatch, useSelector} from 'react-redux';
-import {productDescriptionAction, termBrandAction} from '../actions/brands.action';
-import {getBrandsState} from '../reducer/brands.reducer';
-import {giftCardsUnitAction} from '../actions/gitCards.actions';
-import {getGiftcardsState} from '../reducer/giftCards.reducer';
+import { useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { productDescriptionAction, termBrandAction } from '../actions/brands.action';
+import { getBrandsState } from '../reducer/brands.reducer';
+import { giftCardsUnitAction } from '../actions/gitCards.actions';
+import { getGiftcardsState } from '../reducer/giftCards.reducer';
+import GiftGiftCard from './GiftGiftCard';
 
 
 const SelectCards = () => {
-var eventKey = 1;
-const dispatch = useDispatch();
-const giftunitState = useSelector(getGiftcardsState);
-const productAndTermState = useSelector(getBrandsState);
-const handleSelect = (eventKey1) =>{
-    eventKey = eventKey1;
-} 
+    
+    const dispatch = useDispatch();
+    const [eventKey11, seteventkey] = useState(1);
+    const [tempvisible, setTempVisible] = useState(true);
+    const[count,setcount] = useState(1);
+    const giftunitState = useSelector(getGiftcardsState);
+    const productAndTermState = useSelector(getBrandsState);
+    const handleSelect = (eventKey1) => {
+        seteventkey(eventKey1);
+    }
+    const changeHandler = () =>{
+        setTempVisible(!tempvisible)
+    }
+    const increment = () => {
+       count >=5? setcount(5):setcount(count+1);
+    }
+    const decrement = () => {
+        count >1 ?setcount(count-1):setcount(1)
+    }
 
-React.useEffect(() => {
-    dispatch(productDescriptionAction({
-        currency:1,
-        program_id:1,
+    React.useEffect(() => {
+        dispatch(productDescriptionAction({
+            currency: 1,
+            program_id: 1,
         }))
-        // dispatch(termBrandAction({
+    }, [])
 
-        // }))
-
-    // }, [])
-
-}, [])
-
-React.useEffect(() => {
-    dispatch(giftCardsUnitAction({
-        currency: giftunitState.giftunit_id,
-        program_id: 1,
-        giftunit_id: giftunitState.giftunit_id
-    }))
-}, [giftunitState.giftunit_id])
+    React.useEffect(() => {
+        dispatch(giftCardsUnitAction({
+            currency: giftunitState.giftunit_id,
+            program_id: 1,
+            giftunit_id: giftunitState.giftunit_id
+        }))
+    }, [giftunitState.giftunit_id])
 
     return (
 
@@ -61,38 +69,39 @@ React.useEffect(() => {
                     </div>
                     <p className="select-card-text mt-5">Gifting for</p>
                     <div className="row mr-sm-3 mt-3 mb-3">
-                        <Form.Check type="radio" className="giftslabs" label="Myself" name="formHorizontalRadios" id="formHorizontalRadios1" />
-                        <Form.Check type="radio" className="giftslabs" label="Someone else" name="formHorizontalRadios" id="formHorizontalRadios2" />
+                        <Form.Check type="radio" className="giftslabs" label="Myself" name="formHorizontalRadios" id="formHorizontalRadios1" onClick ={changeHandler} checked ={tempvisible == true} />
+                        <Form.Check type="radio" className="giftslabs" label="Someone else" name="formHorizontalRadios" id="formHorizontalRadios2" onClick ={changeHandler}/>
                     </div>
+                    {tempvisible == false ?( <GiftGiftCard />):''}
                     <div>
-                    <Nav activeKey="1" onSelect={handleSelect}>
-                        <Nav.Item id="product" >
-                            <Nav.Link eventKey="1" >Description</Nav.Link>
-                        </Nav.Item>
-                        <Nav.Item ClassName="product" >
-                            <Nav.Link eventKey="2">Terms & Condtions</Nav.Link>
-                        </Nav.Item>
+                        <Nav onSelect={handleSelect}>
+                            <Nav.Item id="product" >
+                                <Nav.Link eventKey="1" active={eventKey11 == "1"}>Description</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item id="product" >
+                                <Nav.Link eventKey="2">Terms & Condtions</Nav.Link>
+                            </Nav.Item>
                         </Nav>
-                        {eventKey == "1" ? (
-                          <div id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                          <p>Amazon.com.au Gift Cards is the perfect gift of choice! It gives your recipient the ability to choose from millions of items sold on<br />
+                        {eventKey11 == "1" ? (
+                            <div className="mt-4" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                                <p ClassName="mt-4">Amazon.com.au Gift Cards is the perfect gift of choice! It gives your recipient the ability to choose from millions of items sold on<br />
                       Amazon.com.au – from electronics to fashion to appliances and household items.</p>
-                          <p className="mt-4">Your giftee can use their Amazon.com Gift Card towards books, electronics, music, and more. The Amazon.com.au website is the place to find and discover almost anything you want to buy online at a great price.</p>
-                          <p>spend hours thinking about whether to buy your recipient something for their home, their wardrobe or for their travels. Or you could open their doors of possibility by treating them to an Amazon.com.au gift card from Prezzee, which will give them a world of choice and save you a lot of time! What’s more, by choosing a Amazon.com.au eGift Card from Prezzee, your gift card is ready to send in moments after your order, so they’re absolutely perfect for last minute gifts. Choose from SMS or email delivery – all gift cards are stored and managed digitally, meaning no lost or worse still, gift cards that have expired because you’ve forgotten all about them. Your giftee will have 24/7 access to their wonderful gift right on their mobile phone – how thoughtful is that!? Amazon.com.au Gift Cards carry no fees and do not expire.</p>
+                                <p className="mt-4">Your giftee can use their Amazon.com Gift Card towards books, electronics, music, and more. The Amazon.com.au website is the place to find and discover almost anything you want to buy online at a great price.</p>
+                                <p>spend hours thinking about whether to buy your recipient something for their home, their wardrobe or for their travels. Or you could open their doors of possibility by treating them to an Amazon.com.au gift card from Prezzee, which will give them a world of choice and save you a lot of time! What’s more, by choosing a Amazon.com.au eGift Card from Prezzee, your gift card is ready to send in moments after your order, so they’re absolutely perfect for last minute gifts. Choose from SMS or email delivery – all gift cards are stored and managed digitally, meaning no lost or worse still, gift cards that have expired because you’ve forgotten all about them. Your giftee will have 24/7 access to their wonderful gift right on their mobile phone – how thoughtful is that!? Amazon.com.au Gift Cards carry no fees and do not expire.</p>
 
-                      </div>
-  ) : (
-    <div id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                            </div>
+                        ) : (
+                            <div className="mt-4" id="contact" role="tabpanel" aria-labelledby="contact-tab">
 
-    <p>Amazon.com.au Gift Cards is the perfect gift of choice! It gives your recipient the ability to choose from millions of items sold on<br />
+                                <p>terms & conditions tAmazon.com.au Gift Cards is the perfect gift of choice! It gives your recipient the ability to choose from millions of items sold on<br />
     Amazon.com.au – from electronics to fashion to appliances and household items.</p>
-    <p className="mt-4">Your giftee can use their Amazon.com Gift Card towards books, electronics, music, and more. The Amazon.com.au website is the place to find and discover almost anything you want to buy online at a great price.</p>
-    <p>spend hours thinking about whether to buy your recipient something for their home, their wardrobe or for their travels. Or you could open their doors of possibility by treating them to an Amazon.com.au gift card from Prezzee, which will give them a world of choice and save you a lot of time! What’s more, by choosing a Amazon.com.au eGift Card from Prezzee, your gift card is ready to send in moments after your order, so they’re absolutely perfect for last minute gifts. Choose from SMS or email delivery – all gift cards are stored and managed digitally, meaning no lost or worse still, gift cards that have expired because you’ve forgotten all about them. Your giftee will have 24/7 access to their wonderful gift right on their mobile phone – how thoughtful is that!? Amazon.com.au Gift Cards carry no fees and do not expire.</p>
+                                <p className="mt-4">Your giftee can use their Amazon.com Gift Card towards books, electronics, music, and more. The Amazon.com.au website is the place to find and discover almost anything you want to buy online at a great price.</p>
+                                <p>spend hours thinking about whether to buy your recipient something for their home, their wardrobe or for their travels. Or you could open their doors of possibility by treating them to an Amazon.com.au gift card from Prezzee, which will give them a world of choice and save you a lot of time! What’s more, by choosing a Amazon.com.au eGift Card from Prezzee, your gift card is ready to send in moments after your order, so they’re absolutely perfect for last minute gifts. Choose from SMS or email delivery – all gift cards are stored and managed digitally, meaning no lost or worse still, gift cards that have expired because you’ve forgotten all about them. Your giftee will have 24/7 access to their wonderful gift right on their mobile phone – how thoughtful is that!? Amazon.com.au Gift Cards carry no fees and do not expire.</p>
 
-</div>
-  )}
- 
-</div>
+                            </div>
+                        )}
+
+                    </div>
 
 
                 </div>
@@ -104,7 +113,7 @@ React.useEffect(() => {
                     <h4 className="ml-sm-2 amttext2">AED 250</h4>
                     <div className="col mr-5">
                         <ButtonGroup className="mr-3" aria-label="Second group">
-                            <Button variant="light"> <img src={minusicon} /></Button> <Button variant="light">1</Button> <Button variant="light"> <img src={plusicon} /></Button>
+                            <Button variant="light" onClick ={decrement}> <img src={minusicon} /></Button> <Button variant="light">{count}</Button> <Button variant="light" onClick ={increment}> <img src={plusicon} /></Button>
                         </ButtonGroup>
                         <Button className="nav-btn mr-2 text-white">Add to cart</Button>{' '}
                         <Button className="nav-btn mr-2" variant="info">Buy Now</Button>{' '}
