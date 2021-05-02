f8621c696c84c05bd002b76a510fc05d87f869d1
shreya_19 <shreya@visions.net.in>
2021-05-03 01:21:14 +0530

    sign_in_api

--- client/src/components/IDC/IdcSign.js
+++ client/src/components/IDC/IdcSign.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React , { useState, useEffect } from 'react';
 import './Idc.css';
 import IDC_LOGO_white_icon_1 from '../../assets/IDC_LOGO_white_icon_1.svg';
 import Slider from '../../assets/Slider.png';
+import { useHistory } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import {IdcSignInAction} from '../../actions/idc_action';
 
 const Idc_Signin = () => {
+    const dispatch = useDispatch();
+    // const state = useSelector(getAuthState);
+    const history = useHistory();
+    const [idcIsValid, setIdcIsValid] = useState(false);
+    const [idcVisible, setIdcVisible] = useState(true);
+
+    const formik = useFormik({
+        initialValues: {
+          email: "",
+          password: "",
+        },
+        validationSchema: Yup.object({
+          email: Yup.string().min(2).max(200).email().required(),
+          password: Yup.string().min(2).max(200).required(),
+        }),
+        onSubmit: (data) => {
+          dispatch(IdcSignInAction(data));
+        },
+      });
+
+
+
+
     return (
         <div className="signInLayout1">
     <div className="container-fluid px-0">
@@ -19,13 +47,14 @@ const Idc_Signin = () => {
                     <h2>Welcome Back</h2>
                     <p>Login to manage your account.</p>
                     <div className="formLoginWrapper">
-                        <form name="idc_order" role="form" novalidate>
+                        <form name="idc_order" role="form" onSubmit={formik.handleSubmit} novalidate>
                             <div className="form-group">
                                 <label className="customL">
                                     <span>EMAIL ADDRESS</span>
                                 </label>
                                 <input type="email" placeholder="{{ 'Email address' | translate }}" className="form-control"
-                                autocomplete="off" ng-model="username" name="username" ng-required="true"/>
+                                autocomplete="off" value={formik.values.email}
+                                onChange={formik.handleChange} name="email" ng-required="true"/>
                                 <p className="help-block" ng-if="idc_order.username.$empty && idc_order.username.$dirty">Please enter a
                                     valid Email Address </p>
                             </div>
@@ -35,11 +64,12 @@ const Idc_Signin = () => {
                                     <a ng-click="dialogForgotPassword1( )" className="forgotURL">Forgot Password</a>
                                 </label>
                                 <input type="password" ng-model="password" name="password" required
-                                autocomplete="off"placeholder="Password" className="form-control"/>
+                                autocomplete="off" value={formik.values.password}
+                                onChange={formik.handleChange} placeholder="Password" className="form-control"/>
                             </div>
                             <div className="btn-layout1 text-center">
                                 <button className="btn" type="submit"
-                                    ng-click="idcSignIn()" ng-disabled="idc_order.$invalid">Log in</button>
+                                    ng-disabled="idc_order.$invalid">Log in</button>
                             </div>
                         </form>
                         <div className="borderSpaceWhite">
